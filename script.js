12894a7df59d9350491c783847b85223e40241c3
Hugo Bigatto <59802010+hbigatto@users.noreply.github.com>
2023-01-08 11:33:05 -0500

    Additions made
    Added longitude , latitude and feels like to the information displayed.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let weather = {
   apiKey: "1f050409a5416d907f466e2dc4ef7607",
+
   fetchWeather: function (city) {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -16,27 +17,33 @@ let weather = {
       })
       .then((data) => this.displayWeather(data));
   },
+
   displayWeather: function (data) {
+    console.log(data);
     const { name } = data;
     const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
+    const { temp, humidity, feels_like } = data.main;
     const { speed } = data.wind;
+    const { lat,lon } = data.coord;
+
     document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".lat").innerText = "Latitude: " + lat;
+    document.querySelector(".long").innerText = "Longitude: " + lon;
+    document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".humidity").innerText =
-      "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText =
-      "Wind speed: " + speed + " km/h";
+    document.querySelector(".humidity").innerText ="Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText ="Wind speed: " + speed + " km/h";
+    document.querySelector(".feels").innerText = "Feels Like: " + feels_like;
     document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + name + "')";
+
   },
+
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
   },
+
 };
 
 document.querySelector(".search button").addEventListener("click", function () {
